5e61b8dff6545bc1e2922d44aa7b0b602708dd9b
Logan Demmy <frank.demmy@gmail.com>
2021-08-18 17:27:19 -0500

    created the entity framework

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        //This id is an entity that is referenced by the student thus it is named EnrollentID instead of ID
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        // the ? means that this is a nullable value
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
